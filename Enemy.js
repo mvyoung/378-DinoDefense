c4eb973358f685024450227b1214b2636537e660
Trevor <tdevore7@gmail.com>
2013-12-07 18:52:35 -0800

    updated enemy class

--- Enemy.js
+++ Enemy.js
@@ -1,25 +1,14 @@
-Enemy = Class.create(Sprite, // extend the sprite class
+Enemy = Class.create(Sprite, 
 {
-    initialize: function(x, y, enemyType) { //initialization
-        Sprite.call(this, 64, 64); //initialize the sprite object
-		this.frame = 0;
-        this.image = game.assets['?.png']; //load image asset
-        this.moveSpeed = 2;
-		this.health = 1;
-		this.isSlowed = false;
-		
-		
+    initialize: function() {
+        Sprite.call(this, gridPx, gridPx); //initialize the sprite object
+        this.isSlowed = false;
         this.x = x;
         this.y = y;
+        
         this.key = 0;
-		
-		if (enemyType == 0) {
-			this.image = game.assets['?.png'];
-			this.movespeed = 2;
-			this.health = 10;
-		}
-		
-        game.rootScene.addChild(this);
+        
+        //game.rootScene.addChild(this); //needed?
     },
     remove: function() {
         game.rootScene.removeChild(this);
@@ -28,14 +17,31 @@ Enemy = Class.create(Sprite, // extend the sprite class
     },
     //define the enterframe event listener
     onenterframe: function() {
-		if (this.isSlowed) {
-			this.x += this.moveSpeed/2;
-		} else {
-			this.x += this.moveSpeed;
-		}
+        if (this.isSlowed) {
+            this.x += this.moveSpeed/2;
+        } else {
+            this.x += this.moveSpeed;
+        }
         
-        if (this.x < -64) {
+        // Remove if out of view
+        if (this.x > 664) {
             this.remove();
         }
     },
 });
+
+StandardDino = Class.create(Enemy, {
+    initialize: function(x, y) {
+        Enemy.call(this);
+        this.image = game.assets['chara1.png'];
+        this.frame = 0;
+        
+        this.key = 0;
+        
+        this.health = 10;
+        this.moveSpeed = 4;
+        this.isSlowed = false;
+        this.x = x;
+        this.y = y;
+    }
+}
