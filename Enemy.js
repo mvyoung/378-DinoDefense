c684994c81a9b1d1f7bddee75cc8f86ad2b176c2
Trevor <tdevore7@gmail.com>
2013-12-07 21:35:07 -0800

    minor updates to Enemy class

--- Enemy.js
+++ Enemy.js
@@ -1,28 +1,30 @@
 Enemy = Class.create(Sprite, // extend the sprite class
 {
     initialize: function(x, y, enemyType) { //initialization
-        Sprite.call(this, 64, 64); //initialize the sprite object
-		this.frame = 0;
+        Sprite.call(this, 40, 40); //initialize the sprite object
+		this.frame = 1;
         //this.image = game.assets['chara1.png']; //load image asset
-        this.moveSpeed = 2;
+        this.moveSpeed = 5;
 		this.health = 1;
 		this.isSlowed = false;
 		
 		
         this.x = x;
         this.y = y;
+        this.gridX = 0;
+        this.gridY = 9;
         this.key = 0;
 		
 		if (enemyType == 0) {
-			this.image = game.assets['images/chara1.png'];
+			this.image = game.assets['images/grid.png'];
 			this.movespeed = 2;
 			this.health = 10;
 		}
-		game.currentScene.addChild(this);
+		game.currentScene.insertBefore(this, game.currentScene.toolbar);
         //game..addChild(this);
     },
     remove: function() {
-        game.rootScene.removeChild(this);
+        game.currentScene.removeChild(this);
         delete enemies [this.key]; //TODO: handle this
         delete this;
     },
@@ -34,7 +36,7 @@ Enemy = Class.create(Sprite, // extend the sprite class
 			this.x += this.moveSpeed;
 		}
         
-        if (this.x < -64) {
+        if (this.x > 840) {
             this.remove();
         }
     },
--- Stage.js
+++ Stage.js
@@ -12,7 +12,8 @@ Stage = Class.create(Scene, {
    onenterframe: function() {
 	   if (this.age % 200 === 0) {
            for (i = 0; i < 1; i++) {
-		      var d = new Enemy(40, 200 + (i *50), 0); 
+		      var d = new Enemy(-40, 9 * gridPx, 0);
+              d.key = i;
 		      //this.addChild(d);
 			  enemies[i] = d;
 		   }
