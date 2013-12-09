ca8478f101cfa7d64c769ef0d003256ea61a49b6
mvyoung <miles.young0@gmail.com>
2013-12-09 05:45:27 -0800

    difficulty levels implemented

--- Start.js
+++ Start.js
@@ -4,11 +4,15 @@ Start = Class.create(Scene, {
 
       tutorial = new Tutorial(800, 1000);
       game_title = new Game_title(356, 194);
-      play = new Play(197, 67);
+      option_easy = new Play_Easy(200, 65);
+      option_medium = new Play_Medium(200, 65);
+      option_hard = new Play_Hard(200, 65)
 
       this.addChild(tutorial);
       this.addChild(game_title);
-      this.addChild(play);
+      this.addChild(option_easy);
+      this.addChild(option_medium);
+      this.addChild(option_hard);
    }
 });
 
@@ -24,19 +28,57 @@ Game_title = Class.create(Sprite, {
     Sprite.call(this, x, y);
     this.image = game.assets['images/title.png'];
     this.x = 290;
-    this.y = 200;
+    this.y = 50;
   }
 });
 
-Play = Class.create(Sprite, {
+Play_Easy = Class.create(Sprite, {
   initialize: function(x, y) {
     Sprite.call(this, x, y);
-    this.image = game.assets['images/play.png'];
+    this.image = game.assets['images/level_easy.png'];
     this.x = 350;
-    this.y = 450;
+    this.y = 300;
   },
 
   ontouchend: function() {
     game.popScene();
+
+    var easy_map = new Grid(easy, easy_col);
+    var stage = new Stage(easy_map);
+    game.pushScene(stage);
+  }
+});
+
+Play_Medium = Class.create(Sprite, {
+  initialize: function(x, y) {
+    Sprite.call(this, x, y);
+    this.image = game.assets['images/level_medium.png'];
+    this.x = 350;
+    this.y = 400;
+  },
+
+  ontouchend: function() {
+    game.popScene();
+
+    var medium_map = new Grid(medium, medium_col);
+    var stage = new Stage(medium_map);
+    game.pushScene(stage);
+  }
+});
+
+Play_Hard = Class.create(Sprite, {
+  initialize: function(x, y) {
+    Sprite.call(this, x, y);
+    this.image = game.assets['images/level_hard.png'];
+    this.x = 350;
+    this.y = 500;
+  },
+
+  ontouchend: function() {
+    game.popScene();
+
+    var hard_map = new Grid(hard, hard_col);
+    var stage = new Stage(hard_map);
+    game.pushScene(stage);
   }
 });
